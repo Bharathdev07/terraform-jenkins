82134bed9faa7544eca3c7862167c3665d78e7f9
Bharathdev07 <devops854@gmail.com>
2024-08-08 14:49:55 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,95 @@
+variable "ami" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "insta" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "key" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "vpc" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "range" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "cidr-1" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "pub-sub-1" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "pub-sub-2" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "cidr-2" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "az-1" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "az-2" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "az-3" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "pri-sub-1" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "cidr-3" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "pri-sub-2" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "cidr-4" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "pub-route" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "pri-route" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
+variable "ig-gate" {
+    description="this is type of ami"
+    type=string
+    default=""
+}
